0a82df669fd486e6ad1b072a9c725c85936bb18a
Szabolcs Vasas <vasas@apache.org>
2020-01-05 13:39:59 +0100

    Removing AWS Cognito identity pool id configuration
    * Based on my testing this field is not needed.

--- src/env-config.js
+++ src/env-config.js
@@ -2,7 +2,6 @@ const envToUse = (env.NAME === "$ENV_NAME") ? process.env : env
 
 export const cognitoConfig = {
   "aws_project_region": envToUse.VUE_APP_FLASHBACKER_COGNITO_REGION,
-  "aws_cognito_identity_pool_id": envToUse.VUE_APP_FLASHBACKER_COGNITO_IDENTITY_POOL_ID,
   "aws_cognito_region": envToUse.VUE_APP_FLASHBACKER_COGNITO_REGION,
   "aws_user_pools_id": envToUse.VUE_APP_FLASHBACKER_COGNITO_USER_POOLS_ID,
   "aws_user_pools_web_client_id": envToUse.VUE_APP_FLASHBACKER_COGNITO_USER_POOLS_WEB_CLIENT_ID,
