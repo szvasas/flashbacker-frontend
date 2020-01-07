8520b2b3f6abf16644c3c05e7aed90cbf9f06272
Szabolcs Vasas <vasas@apache.org>
2020-01-07 15:09:21 +0000

    BrowseStories view sorts stories descending by dateHappened and within each dateHappened group by timestampCreated ascending.

--- src/views/BrowseEntries.vue
+++ src/views/BrowseEntries.vue
@@ -35,10 +35,28 @@
         let response = await restClient.get(endpointUrl);
         let embeddedData = response.data._embedded
         if (embeddedData) {
-          this.loadedStories = [...embeddedData.stories.reverse()]
+          this.loadedStories = [...this.sortStories(embeddedData.stories)]
         } else {
           this.loadedStories = []
         }
+      },
+      // The method sorts descending by dateHappened and within each dateHappened group by timestampCreated ascending.
+      sortStories(storiesArray) {
+        return storiesArray.sort(function (s1, s2) {
+          if (s1.dateHappened > s2.dateHappened) {
+            return -1;
+          } else if (s2.dateHappened > s1.dateHappened) {
+            return 1;
+          } else {
+            if (s1.timestampCreated < s2.timestampCreated) {
+              return -1;
+            } else if (s2.timestampCreated < s1.timestampCreated) {
+              return 1;
+            } else {
+              return 0
+            }
+          }
+        })
       }
     }
   }
